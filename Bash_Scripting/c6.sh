48aeea602433cc149e812c828695917bca347150
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-05-25 12:32:18 -0500

    Create c6.sh

---
+++ Bash_Scripting/c6.sh
@@ -0,0 +1,13 @@
+# TPS REPORTS
+# Using a Bash script, generate an email message that reminds the user to submit a TPS report by a certain day. Your script should accept a date or day
+# as an input argument. For example, if the user inputs "Tuesday", then the output should look something like:
+
+# "Please submit your TPS reports by EOD Tuesday. Your cooperation in this matter is much appreciated."
+
+# For an extra challenge, have your script accept arguments for:
+# * Name of recipient(s)
+# * Due date for TPS reports
+# * Motivational closing
+# * Sender
+
+# If any of the arguments are blank, the email should output a default word or phrase.
