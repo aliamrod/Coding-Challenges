4a89e3684b1cf162eb6c8a531464c54ddcd0ca59
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-05-25 12:03:27 -0500

    Create c4.sh

---
+++ Bash_Scripting/c4.sh
@@ -0,0 +1,6 @@
+# SED COMMANDS
+# The stream editor (SED) command is widely used by Bash programmers to process and modify test. 
+# Write a Bash script that includes an SED command that takes the files in your home directory and changes their owner from your username to 
+# the reverse of your username. So, for example, if your username is alia-mrod, then your Bash script should change the owner to dorm-aila.
+
+# For an extra challenge, have your script accept a numerical input, 'n', that changes the owner of just the first 'n' files in your home directory.
