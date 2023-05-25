12b738540a09b904ca09d44c416c1f7c46ac8fa2
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-05-25 12:42:45 -0500

    Create c8.sh

---
+++ Bash_Scripting/c8.sh
@@ -0,0 +1,10 @@
+# DETECT MOUNTED DISKS
+# Programmers who work in Bash are often System Administrators who manage computer and server networks. So, it's important to known which disks and file systems 
+# are mounted -- that is, accessible by the computer's file system. Note that just because a disk or computer is physically attached with cables doesn't always mean
+# that it's mounted and accessible.
+
+# Come up with a Bash script that detects whether a given mount point or file system is mounted. If so, the script should return the amount of used space on the file system 
+# and the amount of free space. If the file system isn't mounted, your script should return an error message.
+
+# For an added challenge, add a prompt to your script that asks the user if they want to attempt to mount the file system if it's unmounted. 
+# When the file system mounts, the script should then output the amount of used space and free space in the file system.
