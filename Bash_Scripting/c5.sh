f06bd9ac318a496503d5807b9ffaa57c892b3b0e
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-05-25 12:07:01 -0500

    Update c5.sh

--- Bash_Scripting/c5.sh
+++ Bash_Scripting/c5.sh
@@ -1,4 +1,9 @@
 # AWK COMMANDS
 # Another way to work with text in Bash is with AWK commands. For this challenge we will use the following dataset of the world's countries and their capitals.
-# Link:
+# Link: https://github.com/aliamrod/Coding-Challenges/blob/main/Bash_Scripting/c5_supplementary.csv
+
+# Create a Bash script that outputs this dataset into two columns: one for the country and the other for its capital.
+
+# For an added challenge, have your script accept a letter as input. The output should be just the countries that start with the input letter.
+# For another challenge, create another Bash script that outputs a two-column list sorted alphabetically by capital city.
 
