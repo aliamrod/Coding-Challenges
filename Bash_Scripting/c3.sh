2b12bdb70d3657a66e2df77eff55e00d18ecd673
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-05-25 11:58:34 -0500

    Create c3.sh

---
+++ Bash_Scripting/c3.sh
@@ -0,0 +1,7 @@
+# LISTING AND SORTING DIRECTORIES
+# Create a Bash script that accepts directory paths as arguments. For each argument, list the contents of each directory with the name of each listed at the top.
+
+# If you want to give yourself an added challenge, have your script accept an additional argument, "a" or "d", that sorts the contents of
+# each directory in either ascending or descending order. For even more of a challenge, create a Bash script that sorts the contents of the directories
+# based on an "a" or "d" argument that directly precedes it. In other words, you should be able to sort one directory in ascending order, 
+# a second directory in descending order, and so on.
