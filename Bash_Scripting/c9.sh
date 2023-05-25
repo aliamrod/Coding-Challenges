dc22ba8b72c9d730fcb206897bfd400c4f4fa98a
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-05-25 12:37:33 -0500

    Create c9.sh

---
+++ Bash_Scripting/c9.sh
@@ -0,0 +1,8 @@
+# RECURSION COUNTDOWN
+# Recursion is when a function calls itself to perform the same task until some condition is met. This is most commonly done in Bash with loops.
+
+# Create a countdown generator in Bash that accepts a positive integer as input. The output should show numbers decreasing, starting with the input number until 0.
+# After displaying 0, the output should also display "Blast off!". 
+
+# Looking for something a little more challenging, try creating a Bash function that accepts any number of arguments. The output should print out
+# each argument on a new line. Your Bash script should use the echo command only once.
