10533022a6f6b032517fcd82882eb2db7dcfa339
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-05-25 12:05:05 -0500

    Create c5.sh

---
+++ Bash_Scripting/c5.sh
@@ -0,0 +1,4 @@
+# AWK COMMANDS
+# Another way to work with text in Bash is with AWK commands. For this challenge we will use the following dataset of the world's countries and their capitals.
+# Link:
+
