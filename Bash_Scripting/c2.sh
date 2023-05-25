5ccaf79e2e065320b39ffa78e20b1213adcf1351
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-05-25 11:53:39 -0500

    Create c2.sh

---
+++ Bash_Scripting/c2.sh
@@ -0,0 +1,13 @@
+# "PICK YOUR FAVORITE"
+# Think of three things you like and create a Bash script that prompts the user to respond based on these things.
+# It could be something like "Which would you choose..."
+
+# Using case statements, have the script output different responses based on the chosen selection. For example, 
+# if cake is a choice and someone picks cake, a sample output response could be "Great choice. Enjoy your cake!"
+
+# To make things a bit more challenging, add an appropriate response message if someone picks something that isn't on the list.
+# For example, "Sorry, but that's not on the list. Pick X, Y, or Z, please."
+
+# You can also have your Bash script prompt for more information about the chosen thing. For instance, if someone picks cake, another prompt
+# can ask "What kind of cake?" and that prompt could be followed by a final response based on this input, such as, "Great choice.
+# Enjoy your cake!"
