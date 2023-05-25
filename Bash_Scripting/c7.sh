18dedf342b63be047eb633136b349f617dfac755
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-05-25 12:44:50 -0500

    Create c7.sh

---
+++ Bash_Scripting/c7.sh
@@ -0,0 +1,8 @@
+# ALARM CLOCK
+# Create a Bash script that "sleeps" for a given number of seconds before beeping after the time has elapsed. You can make this exercise more challenging by
+# adding features to your alarm clock, such as:
+
+# * An input argument that accepts time rather than the number of seconds.
+# * A snooze function that lets you reset the alarm clock to go off again after a given number of minutes.
+# * An option that enables you to choose from different beeping sounds in your computer system.
+# * An option that plays an audio file stored on your computer.
