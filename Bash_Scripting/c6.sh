e07d2ca0024f103d33045502f73d048dbbdc44fe
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-05-26 14:41:13 -0500

    Update c6.sh

--- Bash_Scripting/c6.sh
+++ Bash_Scripting/c6.sh
@@ -11,3 +11,37 @@
 # * Sender
 
 # If any of the arguments are blank, the email should output a default word or phrase.
+
+#! /bin/bash
+
+# Set default values
+recipient="Recipient"
+due_data="EOD"
+day="Tuesday"
+closing="Your cooperation in this matter is much appreciated."
+sender="Sender"
+
+# Check if arguments are provided and assign custom values
+if [ $# -eq 5 ]; then # if number of arguments is equal to 5...
+  recipient="$1"
+  due_date="$2"
+  day="$3"
+  closing="$4"
+  sender="$5"
+fi
+
+# Generate the email message
+echo "Dear $recipient,
+
+This is a friendly reminder to submit your TPS report by $due_date $day. $closing
+
+Best Regards,
+$sender
+
+# This script is saved as `tps_report.sh` and made executable. It is run by executing `./tps_report.sh`, and generates
+# the email message with default values.
+
+# To customize the email, you can provide the following arguments in the specified order:
+# `<Recipient> <Due Date> <Day> <Closing> <Sender>`. For instance,
+# `./tps_report.sh "John Doe" "Friday" "5:00 PM" "Your prompt action is vital." "Jane Smith"` will generate the email message
+# with customized values.
