e4641ecef2f6880f6659246667bcba24ab026fb1
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-05-26 10:42:55 -0500

    Update c4.sh

--- Bash_Scripting/c4.sh
+++ Bash_Scripting/c4.sh
@@ -4,3 +4,15 @@
 # the reverse of your username. So, for example, if your username is alia-mrod, then your Bash script should change the owner to dorm-aila.
 
 # For an extra challenge, have your script accept a numerical input, 'n', that changes the owner of just the first 'n' files in your home directory.
+
+username=$(whoami)
+reverse_username=$(echo "$username" | rev)
+
+# Function to change owner of files in home directory.
+change_owner() {
+  local num_files="$1"
+  
+  if [[! -d "$HOME" ]]; then
+    echo "Home directory not found!"
+    exit 1 
+  fi
