4585568b4a1d838c1d8f437c449c9978a0f6f914
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-09-04 10:34:14 -0500

    Create 10.sql

---
+++ SQL/EASY/10.sql
@@ -0,0 +1,12 @@
+/*
+
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+*/
