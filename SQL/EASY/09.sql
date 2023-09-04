db33596d59f2702182af2ae30039209df9b552a8
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-09-04 10:34:02 -0500

    Create 09.sql

---
+++ SQL/EASY/09.sql
@@ -0,0 +1,13 @@
+/*
+
+
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+*/
