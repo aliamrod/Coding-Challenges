5b9ac92a5f2c4d38445fafb2be109b7305277d26
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-09-04 10:35:33 -0500

    Update 08.sql

--- SQL/EASY/08.sql
+++ SQL/EASY/08.sql
@@ -1,14 +1,14 @@
 /*
-Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is `JPN`. 
-The CITY table is described as follows:
+Query a list of CITY and STATE from the STATION table. The STATION table is described as follows:
 
+      STATION
 +-------------+--------------+
 | Field       | Type         |
 +-------------+--------------+
 | ID          | NUMBER       |
-| NAME        | VARCHAR2(17) |
-| COUNTRYCODE | VARCHAR2(3)  |
-| DISTRICT    | VARCHAR2(20) |
-| POPULATION  | NUMBER       |
+| CITY        | VARCHAR2(21) |
+| STATE       | VARCHAR2(2)  |
+| LAT_N       | NUMBER       |
+| LONG_W      | NUMBER       |
 +-------------+--------------+
 */
