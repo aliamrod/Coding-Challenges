b003284c06b67cb6444147f9375e0f99f63dbd96
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-09-05 17:38:38 -0500

    Update 01.py

--- Python/02_Medium/01.py
+++ Python/02_Medium/01.py
@@ -13,5 +13,19 @@
 #Kevin's vowel beginning word = ANA
 #Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 points.
 
+def minion_game(string):
+  s = len(string)
+  vowel = 0
+  consonant = 0
 
-
+for i in range(s):
+  if string[i] in 'AEIOU':
+    vowel+=(s-i)
+  else:
+    consonant+=(s-i)
+if vowel < consonant:
+  print('Stuart' + str(consonant))
+elif vowel > consonant:
+  print('Kevin' + str(vowel))
+else:
+    print('Draw')
