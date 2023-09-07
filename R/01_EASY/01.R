bb65cb8bb447d00e8b065ddf782a3255ae931127
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-09-07 10:35:51 -0500

    Update 01.R

--- R/01_EASY/01.R
+++ R/01_EASY/01.R
@@ -1,3 +1,5 @@
+# FUNNY STRING
+
 # In this challenge, you will determine whether a string is funny or not. To determine whether a string is funny, 
 # create a copy of the string in reverse e.g. abc-> cba. Iterating through each string, compare the absolute difference
 # in the ascii values of the characters at positions 0 and 1, 1 and 2 and so on to the end. If the list of absolute differences
@@ -19,4 +21,19 @@
 # * string: either Funny or NotFunny
 
 # Input Format
-# The first line contains an integer 'q', the number of queries. The next 'q' lines each contain a string, 's'. 
+# The first line contains an integer 'q', the number of queries. The next 'q' lines each contain a string, 's'.
+
+# Constraints
+# * 1<= q <= 10 
+# * 2<= length of s <= 10,000
+
+library(stringr)
+
+nums<-suppressWarnings(readLines(file("stdin"))) # reads lines of input from the standard input (stdin) and suppresses warnings that may occur during the reading process. 
+
+nums2<-nums[-1] # by exclusing the first element of the 'nums' vector, it effectively removes the first line of input. 
+asc<- function(x){ strtoi(charToRaw(x), 16L)
+                  }
+# helper function to convert a character string into a vector of integers representing the ASCII values of the characters in hexadecimal format.
+ strReverse<- function(x)
+   sapply(
