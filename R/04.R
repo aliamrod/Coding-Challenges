fcc5cd270dd0d420d568e5ef7aefb7171018dec1
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-09-07 12:16:41 -0500

    Update 04.R

--- R/04.R
+++ R/04.R
@@ -5,10 +5,39 @@
 # n = 124
 # Check whether 1, 2, and 4 are divisors of 124. All 3 numbers divide evenly into 124 so return 3 .
 
+# SOLUTION 1
 nums <- scan("stdin", sep = " ")
 n <- nums[1] # number of test cases
 nums2 <- nums[-1] # without the number of test cases
 
+for (i in 1:n){
+  ea = 0
+  length <- floor(log10(abs(nums2[i]))) + 1
+  char<- as.character(nums2[i])
+  x<- is.character(char)
+  char2<-as.numeric(unlist(strsplit(char, "")))
+  for (j in 1:length)
+    {
+    if(char2[j] != 0)
+               {
+                char3<-nums2[i]%%char2[j]
+                 }
+                else if(char2[j] == 0)
+                    {
+                    char3<-1
+                }
+
+            if(char3==0)
+                {
+                    ea=ea+1
+                }
+
+                
+
+            
+            }
+         write.table(ea, sep = "", append=T, row.names = F, col.names = F,quote=F)
+    } 
 
 
 
