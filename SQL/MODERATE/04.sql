25439900d00ce4f2a6c9c8156019ec3d4e7a9cdf
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-09-07 16:27:37 -0500

    Update 04.sql

--- SQL/MODERATE/04.sql
+++ SQL/MODERATE/04.sql
@@ -1,1 +1,20 @@
+/* You are given a table, Functions, containing two columns: X and Y. 
 
++-------------+--------------+
+| Column      | Type         |
++-------------+--------------+
+| X           | Integer      |
+| Y           | Integer      |
++-------------+--------------+
+
+Two pairs (X1, Y1) and (X2, Y2) are said to be symmetric pairs if X1 = Y2 and X2 = Y1. Write a query to output
+all such symmetric pairs in ascending order by the value of X. 
+*/
+
+SELECT f1.X, f1.Y FROM Functions AS f1 
+WHERE f1.X = f1.Y AND
+(SELECT COUNT(*) FROM Functions WHERE X = f1.X AND Y = f1.Y) > 1
+UNION
+SELECT f1.X, f1.Y from Functions AS f1
+WHERE EXISTS(SELECT X, Y FROM Functions WHERE f1.X = Y AND f1.Y = X AND f1.X < X)
+ORDER BY X;
