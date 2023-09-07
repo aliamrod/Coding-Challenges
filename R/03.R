34af7215a42849ab9e6693d535f3bcdff915d710
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-09-07 11:43:29 -0500

    Update 03.R

--- R/03.R
+++ R/03.R
@@ -7,12 +7,8 @@
 
 # Read input from STDIN. Print output to STDOUT. 
 nums <- suppressWarnings(readLines(file("stdin")))
-
-
-nums <- suppressWarnings(readLines(file("stdin")))
 nums <- as.list(as.data.frame(t(nums)))
 
 p1<-dbinom(3, size=6, prob=109/209)+dbinom(4, size=6, prob=109/209)+dbinom(5, size=6, prob=109/209)+dbinom(6, size=6, prob=109/209)
-
 p2<-signif(p1,digits=3)
 write.table(p2, sep = " ", append=T, row.names = F, col.names = F,quote = FALSE,)
