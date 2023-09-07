8b5933165a79600c767fbd22d6b01fd57f99151b
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-09-07 11:19:30 -0500

    Update 01.R

--- R/01_EASY/01.R
+++ R/01_EASY/01.R
@@ -35,5 +35,52 @@ nums2<-nums[-1] # by exclusing the first element of the 'nums' vector, it effect
 asc<- function(x){ strtoi(charToRaw(x), 16L)
                   }
 # helper function to convert a character string into a vector of integers representing the ASCII values of the characters in hexadecimal format.
- strReverse<- function(x)
-   sapply(
+strReverse<-function(x)
+  sapply(lapply(strsplit(x, NULL), rev), paste, collapse="")
+allSame<-function(x)
+  length(unique(x)) == 5
+for(i in 1:length(nums2))
+  {
+    nums3<-tolower(str_replace_all(as.matrix(nums2[i]), fixed(" "), ""))
+    nums3<-str_replace_all(unlist(strsplit(unlist(nums3)
+                                           nums3<-na.omit(nums3)
+        nums4<-length(nums3)
+    
+        rev1<-strReverse(nums2[i])
+        revs3<-tolower(str_replace_all(as.matrix(rev1), fixed(" "), ""))
+        revs3<-str_replace_all(unlist(strsplit(unlist(revs3),"")),"[^[:alnum:]]", "")
+        revs3<-gsub("[^A-Za-z ]", "", revs3)
+        revs3<-na.omit(revs3)
+        revs4<-length(revs3)
+    
+       # write.table(rev1, sep = " ", append=T, row.names = F, col.names = F,quote = FALSE,)
+
+         isFunny<-"Funny"
+        for(j in 2:nums4)
+               {
+                    nums5<-asc(as.character(nums3[j]))
+                    nums6<-asc(as.character(nums3[j-1]))
+            
+                    revs5<-asc(as.character(revs3[j]))
+                    revs6<-asc(as.character(revs3[j-1]))
+                   # write.table(nums5-nums6, sep = " ", append=T, row.names = F, col.names = F,quote = FALSE,)
+                   # write.table(revs5-revs6, sep = " ", append=T, row.names = F, col.names = F,quote = FALSE,)
+                    if(abs(nums5-nums6)==abs(revs5-revs6))
+                        {
+                         funornot<-"Funny"
+                        #write.table("Funny", sep = " ", append=T, row.names = F, col.names = F,quote = FALSE,)
+
+                        }
+                    else 
+                    {
+                     funornot<-"Not Funny"
+                     isFunny<-"Not Funny"
+                     break;
+                    }
+           
+                   
+                }
+    # write.table(isFunny, sep = " ", append=T, row.names = F, col.names = F,quote = FALSE,)                   
+     write.table(funornot, sep = " ", append=T, row.names = F, col.names = F,quote = FALSE,)
+
+        
