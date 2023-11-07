51be219fa103dcdb881da4b3953bb51b3b46db01
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-11-07 14:37:04 -0600

    Create 13.sql

---
+++ SQL/EASY/13.sql
@@ -0,0 +1,22 @@
+/*
+Query the list of CITY names from STATION which have vowels (a,e,i,o,u) from as both their first and last characters. Your result cannot contain duplicates. 
+
+Input Format:
+The STATION table is described as follows:
+
+      STATION
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| CITY        | VARCHAR2(21) |
+| STATE       | VARCHAR2(2)  |
+| LAT_N       | NUMBER       |
+| LONG_W      | NUMBER       |
++-------------+--------------+
+
+*/
+
+SELECT DISTINCT CITY
+FROM STATION 
+WHERE LOWER(SUBSTR(CITY, 1, 1)) IN ('a', 'e', 'i', 'o', 'u') AND LOWER(SUBSTR(CITY, -1, 1)) IN ('a', 'e', 'i', 'o', 'u'); 
