08de4e012ab66e1640fa8b6907c58c7b0e761333
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-11-07 15:08:47 -0600

    Update 18.sql

--- SQL/EASY/18.sql
+++ SQL/EASY/18.sql
@@ -1,1 +1,17 @@
+/*
+Given a table STATION that holds data for five fields namely employee_id, name, months, salary.
++-------------+------------+
+| Column      |   Type     |
++-------------+------------+
+| employee_id | INTEGER    |
+| name        | STRING     |
+| months      | INTEGER    |
+| salary      | INTEGER    |
++-------------+------------+
+ 
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+*/
 
+SELECT NAME 
+FROM EMPLOYEE
+ORDER BY NAME; 
