93d035bbdb2f250d47563ac8b174a264e242c358
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-11-13 10:58:20 -0600

    Update 03.sql

--- SQL/ADVANCED/03.sql
+++ SQL/ADVANCED/03.sql
@@ -1,1 +1,15 @@
+/*
+Find the second expensive order in a table.
+*/
 
+SELECT
+  order_id, 
+  order_amount, 
+FROM
+  orders
+
+ORDER BY
+  order_amount DESC
+LIMIT 1 OFFSET 1; 
+
+/*modifying query to LIMIT 1 OFFSET 1 removes first row (OFFSET) and demonstrates 1 row (LIMIT) */
