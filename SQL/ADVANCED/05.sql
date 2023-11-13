4814e9fb1dd2cab03861bd9e5958c63fa4bd05d1
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-11-13 11:10:16 -0600

    Update 05.sql

--- SQL/ADVANCED/05.sql
+++ SQL/ADVANCED/05.sql
@@ -1,1 +1,11 @@
 
+/* Calculate the export and import for each company.*/
+
+SELECT
+  company_id, 
+  SUM(CASE WHEN transaction_type = 'Export' THEN amount ELSE 0 END) AS total_export, 
+  SUM(CASE WHEN transaction_type = 'Import' THEN amount ELSE 0 END) AS total_import
+FROM  
+  transactions
+GROUP BY
+  company_id; 
