77a732b39ea9f6ee131abf51f331fd834c732f24
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-11-13 10:59:31 -0600

    Update 04.sql

--- SQL/ADVANCED/04.sql
+++ SQL/ADVANCED/04.sql
@@ -1,1 +1,12 @@
 
+/* Calculate the toal sales for each store, considering only orders in the year 1993.*/
+
+SELECT
+  store_id, 
+  SUM(sales_amount) AS total_sales_1993
+FROM
+  sales
+WHERE
+  YEAR(order_date) = 1993
+GROUP BY
+  store_id; 
