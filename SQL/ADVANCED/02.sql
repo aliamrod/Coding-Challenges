72aefebd432539f7fcb3cb50ae2e53ec4ed5db05
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-11-13 10:51:45 -0600

    Update 02.sql

--- SQL/ADVANCED/02.sql
+++ SQL/ADVANCED/02.sql
@@ -1,1 +1,11 @@
+/*
+Calculate the percentage contribution of each store to total sales:
+*/
 
+SELECR 
+  store_id, 
+  SUM(sales_amount) / (SELECT SUM(sales_amount) FROM sales) * 100 AS percentage_contribution
+FROM
+  sales
+GROUP BY
+  store_id; 
